b42d367cb9347041d2decf9a72f6a0a421c5d2ba
fg <hfgh>
2024-03-20 17:38:26 +0300

    d

--- Tab01_2/Tab01_2.cpp
+++ Tab01_2/Tab01_2.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <string>
+#include <stdlib.h>
+#include <cstdlib>
 
 
 using namespace std;
@@ -10,7 +12,7 @@ struct date {
 	unsigned short year;
 };
 
-struct line_n {
+struct Line_n {
 	char name[20];
 	char prud[15];
 	unsigned short cou;
@@ -41,7 +43,7 @@ void p_date(unsigned short day, unsigned short month, unsigned short year, int l
 	cout.width(spac); cout << " ";
 }
 
-void c_Tab(struct line_n* line_n) {
+void c_Tab(struct Line_n* line_n) {
 	cout << endl;	cout.width(79 + 5); cout.fill('-'); cout << "-" << endl;
 	cout.fill(' '); cout.width(78 + 5);  cout << left << "|Офисные пакеты"; cout << "|" << endl;
 	cout.width(79 + 5); cout.fill('-'); cout << "-" << endl;
@@ -69,7 +71,7 @@ void c_Tab(struct line_n* line_n) {
 	cout.width(79 + 5); cout.fill('-'); cout << "-" << endl;
 }
 
-void reps(struct line_n* line_n) {
+void reps(struct Line_n* line_n) {
 	int mina = 0;
 	int maxa = 0;
 	for (int i = 1; i < 3; i++) {
@@ -87,16 +89,14 @@ void reps(struct line_n* line_n) {
 	line_n[mina] = line_n[maxa];
 	line_n[maxa] = line_n[4];
 
-	cout << mina;
-	cout << maxa;
-
 }
+
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	struct line_n line_n[10];
+	struct Line_n line_n[10];
 	line_n[0] = { "Office", "Microsoft", 4, 870.99, {11,01,2011} };
 	line_n[1] = { "SmartSute", "Lotus", 5, 1020.99, {21,10,2012} };
 	line_n[2] = { "StarOffice", "Sun", 4, 9.99, {21,10,2020} };
@@ -106,5 +106,45 @@ int main()
 	reps(line_n);
 	
 	c_Tab(line_n);
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+
+	int n = 3; //kol-vo yach
+	Line_n* a;
+	a = (Line_n*)calloc(n, sizeof(Line_n));
+	for (int i = 0; i < n; i++) {
+		strcpy_s(a[i].name, line_n[i].name);
+		strcpy_s(a[i].prud, line_n[i].prud);
+		a[i].cost = line_n[i].cost;
+
+	}
+	Line_n** b;
+	b = (Line_n**) new Line_n * [10];
+	for (int i = 0; i < 10; i++) {
+		b[i] = (Line_n*) new Line_n;
+		*(b[i]) = a[i];
+	}
+	a = (Line_n*)realloc(a, 10 * sizeof(Line_n));
+	if (a) {
+		for (int i = 0; i < 3; i++) {
+			cout << endl << "адрес А[i]" << &a[i] << "A[i] -> name " << a[i].name;
+			cout << ", " << "адрес B[i]" << &b[i] << "B[i] -> name " << b[i]->name;
+		}
+		for (int i = 0; i < 3; i++) {
+			cout << endl << "адрес А[i]" << &a[i] << "A[i] -> prod " << a[i].prud;
+			cout << ", " << "адрес B[i]" << &b[i] << "B[i] -> prod " << b[i]->prud;
+		}
+		for (int i = 0; i < 3; i++) {
+			cout << endl << "адрес А[i]" << &a[i] << "A[i] -> cost " << a[i].cost;
+			cout << ", " << "адрес B[i]" << &b[i] << "B[i] -> cost " << b[i]->cost;
+		}
+		for (int i = 0; i < 10; i++) {
+			delete b[i];
+		}
+	}
+	delete[]b;
+	free(a);
 }
 
